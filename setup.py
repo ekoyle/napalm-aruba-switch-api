78c53984410f58dfa608ef2f99576f25c012c544
Eldon Koyle <ekoyle@gmail.com>
2019-06-28 17:16:07 -0600

    fix name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="napalm-aruba-switch-api-esk",
+    name="napalm-aruba-switch-api",
     version="0.0.1",
     author="Eldon Koyle",
     authon_email="ekoyle@gmail.com",
